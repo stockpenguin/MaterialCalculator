6186624f31b5622901f5725f6d47d5096077b963
stockpenguin <thereallopezmiguel@gmail.com>
2021-05-04 10:33:40 -0500

    Initial commit

---
+++ app/src/main/java/com/stockpenguin/materialcalculator/CalculatorActivity.kt
@@ -0,0 +1,11 @@
+package com.stockpenguin.materialcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
