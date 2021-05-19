2258e10160eada6e8647188584eae23ca20d12ab
Miguel Lopez <thereallopezmiguel@gmail.com>
2021-05-18 23:11:03 -0500

    settinsActivity

--- app/src/main/java/com/stockpenguin/materialcalculator/Compiler.java
+++ app/src/main/java/com/stockpenguin/materialcalculator/Compiler.java
@@ -2,11 +2,22 @@ package com.stockpenguin.materialcalculator;
 
 import java.util.Stack;
 
+/*
+The Compiler class is not really a compiler, but just the backend of how the calculator processes everything
+
+It is a singleton class, although would probably still work if we just used static methods
+ */
+
 public class Compiler {
+    /*
+    since Compiler is a singleton, we have a private static instance
+     */
     private static Compiler instance;
 
+    /* private constructor as well */
     private Compiler() {}
 
+    /* getInstance() method */
     public static Compiler getInstance() {
         if (instance == null) {
             instance = new Compiler();
@@ -14,7 +25,17 @@ public class Compiler {
         return instance;
     }
 
+    /*
+    converts an infix String input and outputs the postfix String
+
+    e.g. "1+2" input = "1 1 +" output
+
+    It uses a stack to successfully create a postfix with 1 traversal
+
+    It throws a SyntaxError as well, only through 1 condition though
+     */
     public String infixToPostfix(String infix) throws SyntaxError {
+        /* StringBuilder so we can append chars throughout the algorithm */
         StringBuilder postfix = new StringBuilder();
         Stack<Character> stack = new Stack<>();
 
--- app/src/main/java/com/stockpenguin/materialcalculator/SettingsActivity.kt
+++ app/src/main/java/com/stockpenguin/materialcalculator/SettingsActivity.kt
@@ -2,10 +2,20 @@ package com.stockpenguin.materialcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var themeManager: ThemeManager
+
+    private lateinit var settingsLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        themeManager = ThemeManager(this)
+        settingsLayout = findViewById(R.id.settingsLayout_SettingsActivity)
+
+        settingsLayout.setBackgroundColor(themeManager.currentThemePrimary)
     }
 }
